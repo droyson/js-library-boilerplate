22422ec0769d3d1f97d7deafa0dda6bd717923fd
Royson <roysondsilva@gmail.com>
2021-08-20 20:23:43 +0530

    jest test not working

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    "ts-jest": {
+      tsconfig: 'tsconfig.json',
+      diagnostics: true
+    }
+  }
+};
